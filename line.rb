6200d5fa68ad286b33187ee84630e493b7677858
Jake Zaia <jzaia@stuy.edu>
2018-02-10 00:45:07 -0500

    Integrate line function into one while loop (sadly not worth it)

--- line.rb
+++ line.rb
@@ -112,6 +112,76 @@ def line(x0, y0, x1, y1, r: 255, g: 255, b: 255)
   end
 end
 
+## Uses 1 while loop. It's actually slower than the other line function.
+def lineINTEGRATED(x0, y0, x1, y1, r: 255, g: 255, b: 255)
+  # x0 is always left of x1
+  if x1 < x0; line(x1, y1, x0, y0, r: r, g: g, b: b) else
+
+    #init vars
+    dy = y1 - y0
+    dx = x1 - x0
+
+    # incrvar = nil    #either x or y, constantly changes by 1
+    # dynamicvar = nil #either x or y, may or may not change
+    # stoppoint = nil  #the point to stop at (incrvar's counterpart)
+    # incrd = nil      #what to increment d by each iteration
+    # dynamicd = nil   #what to increment d by on occaision
+    # octant = nil     #the octant the line is in
+
+    d = 2 * dy - dx
+
+    if dy<0
+      if dy.abs < dx;
+        puts "Drawing line in Octant I" if $DEBUGGING
+        octant = 1
+        incrvar = x0
+        dynamicvar = y0
+        incrd = -2 * dy
+        dynamicd = -2 * dx
+        stoppoint = x1
+      else
+        puts "Drawing line in Octant II" if $DEBUGGING
+        octant = 2
+        incrvar = y0
+        dynamicvar = x0
+        incrd = 2 * dx
+        dynamicd = 2 * dy
+        stoppoint = y1
+      end
+    else
+      if dy > dx;
+        puts "Drawing line in Octant VII" if $DEBUGGING
+        octant = 7
+        incrvar = y0
+        dynamicvar = x0
+        incrd = 2 * dx
+        dynamicd = -2 * dy
+        stoppoint = y1
+      else
+        puts "Drawing line in Octant VIII" if $DEBUGGING
+        octant = 8
+        incrvar = x0
+        dynamicvar = y0
+        incrd = 2 * dy
+        dynamicd = -2 * dx
+        stoppoint = x1
+      end
+    end
+
+    while (incrvar - stoppoint).abs > 0
+      plot(incrvar, dynamicvar, r: r, g: g, b: b) if octant == 1 or octant == 8
+      plot(dynamicvar, incrvar, r: r, g: g, b: b) if octant == 2 or octant == 7
+      if d > 0
+        if octant == 1; dynamicvar-= 1 else
+        dynamicvar += 1 end
+        d+= dynamicd
+      end
+      if octant == 2; incrvar -= 1 else
+      incrvar += 1 end
+      d+= incrd
+    end
+  end
+end
 
 # Define a line by a point, angle and length
 def line_directed(x0, y0, dir, len, r: 255, g: 255, b: 255, cast_down:true)
@@ -130,24 +200,24 @@ end
 ##========================== MAIN ==========================
 
 ##~~~~~~~~~~~~~~~~~~~ Test cases ~~~~~~~~~~~~~~~~~~~
-# line(250, 250, 499, 240, r:0) #Octant I
-# line(250, 250, 499, 200, r:0) #Octant I
-# line(250, 250, 499, 150, r:0) #Octant I
-# line(250, 250, 260, 0, g:0) #Octant II
-# line(250, 250, 350, 0, g:0) #Octant II
-# line(250, 250, 400, 0, g:0) #Octant II
-# line(250, 250, 260, 499, b:0) #Octant VII
-# line(250, 250, 290, 499, b:0) #Octant VII
-# line(250, 250, 400, 499, b:0) #Octant VII
-# line(250, 250, 499, 275) #Octant VIII
-# line(250, 250, 499, 400) #Octant VIII
-# line(250, 250, 499, 450) #Octant VIII
+#  line(250, 250, 499, 240, r:0) #Octant I
+#  line(250, 250, 499, 200, r:0) #Octant I
+#  line(250, 250, 499, 150, r:0) #Octant I
+#  line(250, 250, 260, 0, g:0) #Octant II
+#  line(250, 250, 350, 0, g:0) #Octant II
+#  line(250, 250, 400, 0, g:0) #Octant II
+#  line(250, 250, 260, 499, b:0) #Octant VII
+#  line(250, 250, 290, 499, b:0) #Octant VII
+#  line(250, 250, 400, 499, b:0) #Octant VII
+#  line(250, 250, 499, 275) #Octant VIII
+#  line(250, 250, 499, 400) #Octant VIII
+#  line(250, 250, 499, 450) #Octant VIII
 
 # ## literal edge cases
-# line(0, 499, 499, 0, r:0) #Octant I edge
-# line(250, 499, 250, 0, g:0) #Octant II edge
-# line(0, 0, 499, 499, b:0) #Octant VII edge
-# line(0, 250, 499, 250) #Octant VIII edge
+#  line(0, 499, 499, 0, r:0) #Octant II edge
+#  line(250, 499, 250, 0, g:0) #Octant II edge
+#  line(0, 0, 499, 499, b:0) #Octant VIII edge
+#  line(0, 250, 499, 250) #Octant VIII edge
 
 ##~~~~~~~~~~~~~~~~~~~ Cool Image to show in Gallery ~~~~~~~~~~~~~~~~~~~
 
@@ -164,6 +234,5 @@ while i < TAU
   line_directed(250, 250, i, 75, r: 0, g: 0, b: 0)
   i+= 0.00125 # Needs more precision
 end
-
 
 write_out()
